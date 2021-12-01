b11dc9666110eb7f6d594dfbce04e17bbaf90875
MuajiiTsai <92007378+MuajiiTsai@users.noreply.github.com>
2021-12-02 00:23:41 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -9,7 +9,6 @@ from PIL import Image
 from torchvision import models, transforms
 from torch.utils.data import Dataset, DataLoader, random_split
 from tqdm.notebook import tqdm
-# from google.colab import drive
 import random
 import argparse
 
@@ -17,12 +16,8 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-data", "--datapath")
 args = parser.parse_args()
 
-
 model_path = os.path.join(args.datapath, "brand_new_model.pt")
-# train_path = '/content/drive/My Drive/Deep_Learning/Midterm/train/'
-
-train_path = os.path.join(args.datapath, 'train')
-# raw_label_file = '/content/drive/My Drive/Deep_Learning/Midterm/train.csv'
+train_path = os.path.join(args.datapath, 'train/')
 raw_label_file = os.path.join(args.datapath, 'train.csv') 
 
 img_labels = pd.read_csv(raw_label_file)
@@ -46,6 +41,8 @@ img_with_labels.to_csv(os.path.join(args.datapath, 'img_with_labels.csv'), index
 img_without_labels.to_csv(os.path.join(args.datapath, 'img_without_labels.csv'), index=False)
 img_labels.reset_index(inplace=True)
 
+"""##Random Func"""
+
 #seed
 seed = 1000010
 
@@ -63,7 +60,13 @@ def same_seeds(seed):
     torch.backends.cudnn.deterministic = True
 same_seeds(seed)
 
+"""##Dataset"""
+
 from torchvision.io import read_image, ImageReadMode
+
+Resize = transforms.Compose([
+  transforms.Resize([512,512])
+])
 
 class BoneDataset(Dataset):
     def __init__(self, annotations_file, directory, transform=None, target_transform=None):
@@ -72,7 +75,6 @@ class BoneDataset(Dataset):
         self.img_dir = os.listdir(directory)
         self.transform = transform
         self.target_transform = target_transform
-        # self.item = self.classification()
     def __len__(self):
         return len(self.img_labels)
     def __getitem__(self, idx):
@@ -87,30 +89,14 @@ class BoneDataset(Dataset):
         return image, label
 
 label_file = os.path.join(args.datapath, 'img_with_labels.csv')
-# train_path = '/content/drive/My Drive/Deep_Learning/Midterm/train/'
 
-
-# merge csv
-
-# pseudo_train_file = '/content/drive/My Drive/Deep_Learning/Midterm/pseudo_label.csv'
-
-'''
-pseudo_label = pd.read_csv(pseudo_train_file)
-img_with_labels = pd.read_csv(label_file)
-img_with_labels = img_with_labels.append(pseudo_label, ignore_index=True)
-
-img_with_labels.to_csv('/content/drive/My Drive/Deep_Learning/Midterm/img_with_all_labels.csv', index=False)
-img_with__all_labels_path = '/content/drive/My Drive/Deep_Learning/Midterm/img_with_all_labels.csv'
-'''
 #model
 batch_size = 8
 learning_rate = 1e-4
 num_show = 3
 transform = transforms.Compose([
   transforms.Grayscale(num_output_channels=3),
-  # transforms.PILToTensor(),
   transforms.Resize([512,512]),
-  # transforms.ToPILImage(mode='RGB')
 ])
 
 
@@ -152,24 +138,20 @@ model = model.cuda()
 optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 loss_fn = nn.SmoothL1Loss()
 
-def AUC(y, y_pred):
-# y, y_pred must be numpy array
-  fpr, tpr, thresholds = metrics.roc_curve(y, y_pred, pos_label=1)
-  result = metrics.auc(fpr, tpr)
-  print(result)
-  return result
+"""##Training Model"""
 
 num_epochs = 8
 
 #train the model
-# best_valid_loss = np.inf
+#Uncomment if with valid
+# best_valid_loss = np.inf  
 
 for idx_epoch in range(num_epochs):
     print(f"Epoch{idx_epoch}")
 
     #training phase
     model.train()
-
+    
     train_losses = []
     for image_batch, label_batch in tqdm(train_dataloader):
         optimizer.zero_grad()
@@ -188,87 +170,80 @@ for idx_epoch in range(num_epochs):
     print(f"Training loss: {train_loss}")
     
     #validating phase
-    # model.eval()
+    '''
+    model.eval()
 
-    # valid_losses = []
-    # valid_accuracies = []
-    # valid_label = []
-    # valid_output = []
-    # for image_batch, label_batch in tqdm(validation_dataloader):
-    #     image_batch = image_batch.cuda()
-    #     label_batch = label_batch.cuda()
-    #     output_batch = model(image_batch)
-    #     output_batch = torch.flatten(output_batch)
-    #     # print(output_batch.shape)
-    #     loss = loss_fn(output_batch, label_batch)
+    valid_losses = []
+    valid_accuracies = []
+    valid_label = []
+    valid_output = []
+    for image_batch, label_batch in tqdm(validation_dataloader):
+        image_batch = image_batch.cuda()
+        label_batch = label_batch.cuda()
+        output_batch = model(image_batch)
+        output_batch = torch.flatten(output_batch)
+        loss = loss_fn(output_batch, label_batch)
         
-    #     loss = loss.detach().cpu().numpy()  
-    #     valid_losses.append(loss)
+        loss = loss.detach().cpu().numpy()  
+        valid_losses.append(loss)
 
-    #     output_batch = output_batch.detach().cpu().numpy()
-    #     label_batch = label_batch.detach().cpu().numpy()
-    #     label_batch = np.bool(label_batch)
-    #     # print(f"{output_batch}, {label_batch}")
-    #     valid_label.append(label_batch)
-    #     valid_output.append(output_batch)
+        output_batch = output_batch.detach().cpu().numpy()
+        label_batch = label_batch.detach().cpu().numpy()
+        label_batch = np.bool(label_batch)
+        valid_label.append(label_batch)
+        valid_output.append(output_batch)
 
-    # ##### AUC #####
-    # print(output_batch.shape)
-    # auc = AUC(valid_label, valid_output)
-    # valid_accuracies.append(auc)
+    valid_loss = np.mean(valid_losses)
+    valid_accuracy = np.mean(valid_accuracies)
+    print(f"Testing loss: {valid_loss}")
 
-    # valid_loss = np.mean(valid_losses)
-    # valid_accuracy = np.mean(valid_accuracies)
-    # print(f"Testing loss: {valid_loss}")
-
-    # if valid_loss < best_valid_loss:
-    #     best_valid_loss = valid_loss
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+    '''
     torch.save(model.state_dict(), model_path)
     print("The model is saved")
 
+"""##predict the nonlabel data"""
 
+from torchvision.io import read_image, ImageReadMode
 test_path = 'train/'
-
 nonlabel_img_list = pd.read_csv(os.path.join(args.datapath, 'img_without_labels.csv'))
 
 transform = transforms.Compose([
   transforms.Grayscale(num_output_channels=3),
   transforms.Resize([512,512]),
 ])
+
+
 model.eval()
-# model.cpu()
 test_data = nonlabel_img_list['id']
-# datalist = pd.DataFrame(test_data, columns=['patient_id'])
-# datalist.set_index('patient_id')
 test_output = pd.DataFrame(columns= ['id', 'label'])
 i = 0
 for image_list in tqdm(test_data):
-    # print(image_list)
     if(image_list[-3:] != 'png'):
       continue
-    # print(image_list)
     image = read_image(os.path.join(test_path, image_list), mode=ImageReadMode.RGB)
     image = transform(image)
     image = image.reshape(1, 3, 512, 512)
     image = image.type(torch.FloatTensor)
-    # image = image.cuda()
+    image = image.cuda()
     label_pred = model(image)
     label_pred = label_pred.flatten()
-    label_pred = label_pred.detach().numpy()
+    label_pred = label_pred.cpu().detach().numpy()
     if label_pred>0.5:
       label_pred = np.int16(1)
     else:
       label_pred = np.int16(0)      
-    # print(label_pred)
     new = pd.DataFrame({
             'id': image_list,
             'label': label_pred
         }, index=[i])
-    # print(new)
     test_output = test_output.append(new, ignore_index=True)
     i += 1
     
 test_output.to_csv(os.path.join(args.datapath, 'pseudo_label.csv'), index=False)
+
+"""##with pseudolabel image"""
 
 # merge csv
 
@@ -287,15 +262,13 @@ learning_rate = 1e-4
 num_show = 3
 transform = transforms.Compose([
   transforms.Grayscale(num_output_channels=3),
-  # transforms.PILToTensor(),
   transforms.Resize([512,512]),
-  # transforms.ToPILImage(mode='RGB')
 ])
 
 
 # Split All_train_dataset to train & validation
 
-train_dataset = BoneDataset(label_file, train_path, transform=transform)
+train_dataset = BoneDataset(img_with__all_labels_path, train_path, transform=transform)
 
 ### Turn on must edit train_dataset to All_train_dataset ###
 '''
@@ -319,7 +292,8 @@ validation_dataloader = torch.utils.data.DataLoader(
 
 num_epochs = 8
 
-#train the model
+#train the model with pseudo label
+#Uncomment if with valid
 # best_valid_loss = np.inf
 
 for idx_epoch in range(num_epochs):
@@ -344,42 +318,38 @@ for idx_epoch in range(num_epochs):
 
     train_loss = np.mean(train_losses)
     print(f"Training loss: {train_loss}")
+
     
     #validating phase
-    # model.eval()
+    '''
+    model.eval()
 
-    # valid_losses = []
-    # valid_accuracies = []
-    # valid_label = []
-    # valid_output = []
-    # for image_batch, label_batch in tqdm(validation_dataloader):
-    #     image_batch = image_batch.cuda()
-    #     label_batch = label_batch.cuda()
-    #     output_batch = model(image_batch)
-    #     output_batch = torch.flatten(output_batch)
-    #     # print(output_batch.shape)
-    #     loss = loss_fn(output_batch, label_batch)
+    valid_losses = []
+    valid_accuracies = []
+    valid_label = []
+    valid_output = []
+    for image_batch, label_batch in tqdm(validation_dataloader):
+        image_batch = image_batch.cuda()
+        label_batch = label_batch.cuda()
+        output_batch = model(image_batch)
+        output_batch = torch.flatten(output_batch)
+        loss = loss_fn(output_batch, label_batch)
         
-    #     loss = loss.detach().cpu().numpy()  
-    #     valid_losses.append(loss)
+        loss = loss.detach().cpu().numpy()  
+        valid_losses.append(loss)
 
-    #     output_batch = output_batch.detach().cpu().numpy()
-    #     label_batch = label_batch.detach().cpu().numpy()
-    #     label_batch = np.bool(label_batch)
-    #     # print(f"{output_batch}, {label_batch}")
-    #     valid_label.append(label_batch)
-    #     valid_output.append(output_batch)
+        output_batch = output_batch.detach().cpu().numpy()
+        label_batch = label_batch.detach().cpu().numpy()
+        label_batch = np.bool(label_batch)
+        valid_label.append(label_batch)
+        valid_output.append(output_batch)
 
-    # ##### AUC #####
-    # print(output_batch.shape)
-    # auc = AUC(valid_label, valid_output)
-    # valid_accuracies.append(auc)
+    valid_loss = np.mean(valid_losses)
+    valid_accuracy = np.mean(valid_accuracies)
+    print(f"Testing loss: {valid_loss}")
 
-    # valid_loss = np.mean(valid_losses)
-    # valid_accuracy = np.mean(valid_accuracies)
-    # print(f"Testing loss: {valid_loss}")
-
-    # if valid_loss < best_valid_loss:
-    #     best_valid_loss = valid_loss
+    if valid_loss < best_valid_loss:
+        best_valid_loss = valid_loss
+    '''
     torch.save(model.state_dict(), model_path)
     print("The model is saved")
